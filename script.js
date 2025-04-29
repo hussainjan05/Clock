f40825181c5aa9af058330211499c74c498cb3f0
hussainjan05 <hussainjanafridi5@gmail.com>
2025-04-29 14:17:22 +0500

    OK

---
+++ script.js
@@ -0,0 +1,18 @@
+var second = 0;
+var minute = 0;
+var hour = 0;
+var d = new Date();
+
+
+setInterval(
+    function () {
+        d = new Date();
+        second = d.getSeconds() * 6;
+        minute = d.getMinutes() * 6;
+        hour = d.getHours() * 30 + Math.random( minute / 12);
+  document.getElementById("sec-hand").style.transform = "rotate("+ second + "deg )";
+        
+  document.getElementById("minute-hand").style.transform = "rotate("+ minute + "deg )";
+    
+  document.getElementById("hour-hand").style.transform = "rotate("+ hour + "deg )";
+} ,1000);
